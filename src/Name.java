d9e42e4b5458863156abe267a4b0dfbc429a6924
vaibhav-qait <vaibhavrajeshsingh@qainfotech.com>
2020-05-08 15:58:40 +0530

    Second commit

---
+++ src/Name.java
@@ -0,0 +1,12 @@
+
+public class Name {
+	public static void nameDisplay(String name){
+	System.out.println("My name is " +name);
+	}
+	public static void main(String args[]){
+		String Newname="vaibhav";
+		nameDisplay(Newname);
+	}
+
+
+}
